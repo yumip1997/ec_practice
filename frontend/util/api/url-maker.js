cd693505438f0b60399a50eafcca0ae912965ed0
YUMI <yumip1997@gmail.com>
2022-05-09 16:38:21 +0900

    수정

--- frontend/util/api/url-maker.js
+++ frontend/util/api/url-maker.js
@@ -1,6 +1,6 @@
 const convertURL = (url) =>  getBaseURL() + url
 
-const getBaseURL = () => `${process.server}` ? `${process.env.BASE_URL}` : `${process.env.BROWSER_BASE_URL}`
+const getBaseURL = () => `${process.server}` === "true" ? `${process.env.BASE_URL}` : `${process.env.BROWSER_BASE_URL}`
 
 export {
   convertURL
