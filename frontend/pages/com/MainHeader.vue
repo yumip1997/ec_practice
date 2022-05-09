77b233870407eb1d1fa2615424fca67ef5ced88b
YUMI <yumip1997@gmail.com>
2022-05-09 16:31:09 +0900

    test

--- frontend/pages/com/MainHeader.vue
+++ frontend/pages/com/MainHeader.vue
@@ -5,19 +5,18 @@
 </template>
 
 <script>
-//routing으로 그려지는 화면이 아님 -> asyncData 호출x
-//pages폴더 아래 있는 컴포넌트는 무조건 asyncData을 호출하는 것이 아니라
-//pages 폴더 아래 있는 컴포넌트가 routing 대상 컴포넌트로 자동등록되기에, routing 될 경우에만 asyncData 호출
 import HeaderNav from '~/components/com/nav/HeaderNav.vue';
 import CateApi from "~/util/api/category/cate-api";
 
 export default {
   components: {HeaderNav},
 
-  //TODO fetch에서 호출하면 virturl dom이랑 일치하지 않는 이유 알기
-  async fetch(){
+  async asyncData(){
+    console.log("asnycData called");
     const cateList = await CateApi.getDispCateList();
-    this.cateList = cateList;
+    return{
+      cateList,
+    }
   },
 
   data() {
