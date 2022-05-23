d3ca6d6c8dc5d70c48c68a1f321be9601072f820
YUMI <yumip1997@gmail.com>
2022-05-23 10:41:36 +0900

    수정

--- backend/src/main/java/com/himart/backend/claim/utils/factory/ClaimFactory.java
+++ backend/src/main/java/com/himart/backend/claim/utils/factory/ClaimFactory.java
@@ -5,7 +5,6 @@ import com.himart.backend.claim.utils.core.Claim;
 public class ClaimFactory {
 
     public static Claim create(ClaimType claimType){
-        //T valueOf(String name) : String형 name과 일치하는 열겨형 상수를
         return ClaimType.valueOf(claimType.name()).getClaim();
     }
 
