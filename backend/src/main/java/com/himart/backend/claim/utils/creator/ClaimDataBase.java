833c2343e40e1d6d8a0e80dcaa8cdb9ea4560101
YUMI <yumip1997@gmail.com>
2022-05-25 14:42:16 +0900

    getter 어노테이션 사용

--- backend/src/main/java/com/himart/backend/claim/utils/creator/ClaimDataBase.java
+++ backend/src/main/java/com/himart/backend/claim/utils/creator/ClaimDataBase.java
@@ -1,5 +1,6 @@
 package com.himart.backend.claim.utils.creator;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -8,19 +9,11 @@ public enum ClaimDataBase {
     EA("E", "D", "EA"),
     EC("E", "R", "EC");
 
+    @Getter
     private final String claimCode;
+    @Getter
     private final String deliveryCode;
+    @Getter
     private final String orderStateCode;
 
-    public String getClaimCode(){
-        return claimCode;
-    }
-
-    public String getDeliveryCode(){
-        return deliveryCode;
-    }
-
-    public String getOrderStateCode(){
-        return orderStateCode;
-    }
 }
