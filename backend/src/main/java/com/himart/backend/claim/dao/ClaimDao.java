6b3efdfc116f528b4c86b4fea8c81d9a5d0cc6f7
YUMI <yumip1997@gmail.com>
2022-06-09 13:07:00 +0900

    validation 수정중

--- backend/src/main/java/com/himart/backend/claim/dao/ClaimDao.java
+++ backend/src/main/java/com/himart/backend/claim/dao/ClaimDao.java
@@ -1,11 +1,15 @@
 package com.himart.backend.claim.dao;
 
+import com.himart.backend.claim.com.code.ClaimProcessCode;
 import com.himart.backend.claim.dto.ClaimDto;
 import com.himart.backend.claim.model.ClaimBase;
+import com.himart.backend.test.Claim;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ClaimDao {
 
     ClaimBase selectClaim(ClaimDto claimDto);
+
+    ClaimProcessCode getCurrentClaimProcessCode(ClaimDto claimDto);
 }
--- backend/src/main/java/com/himart/backend/claim/utils/validator/ClaimValidator.java
+++ backend/src/main/java/com/himart/backend/claim/utils/validator/ClaimValidator.java
@@ -1,24 +1,37 @@
 package com.himart.backend.claim.utils.validator;
 
+import com.himart.backend.claim.com.code.ClaimProcessCode;
 import com.himart.backend.claim.dao.ClaimDao;
 import com.himart.backend.claim.dto.ClaimDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Predicate;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class ClaimValidator {
 
     private final ClaimDao claimDao;
 
-    public void isValid(ClaimDto claimDto){
-        this.isStatusValid(claimDto);
-    }
-
-    public void isStatusValid(ClaimDto claimDto){
-        boolean valid = ClaimValidBase.valueOf(claimDto.getClaimType().name()).isValidStatus(claimDto);
-        if(!valid){
-            throw new RuntimeException("클레임 가능 상태가 아닙니다!");
+    public void isValid(ClaimDto claimDto) {
+        try{
+            isValidStatus(claimDto);
+        }catch (Exception e){
+            log.debug(e.getMessage());
         }
     }
+
+    //TODO 간결하게 정리하기
+    private void isValidStatus(ClaimDto claimDto) throws Exception {
+        ClaimProcessCode code = claimDao.getCurrentClaimProcessCode(claimDto);
+
+        ClaimProcessCode currentStatus = ClaimProcessCode.DC;
+        ClaimValidStatus validStatus = ClaimValidStatus.valueOf(claimDto.getClaimType().name());
+        if(validStatus.isContains(currentStatus)) return;
+        throw new Exception("클레임 가능 상태가 아닙니다");
+    }
 }
+
