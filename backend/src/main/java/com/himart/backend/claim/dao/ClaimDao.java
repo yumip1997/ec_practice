dd3992fde139bb3662e30154a9b870e4a80ae8f9
YUMI <yumip1997@gmail.com>
2022-06-09 17:09:55 +0900

    수정

--- backend/src/main/java/com/himart/backend/claim/dao/ClaimDao.java
+++ backend/src/main/java/com/himart/backend/claim/dao/ClaimDao.java
@@ -3,7 +3,6 @@ package com.himart.backend.claim.dao;
 import com.himart.backend.claim.com.code.ClaimProcessCode;
 import com.himart.backend.claim.dto.ClaimDto;
 import com.himart.backend.claim.model.ClaimBase;
-import com.himart.backend.test.Claim;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
