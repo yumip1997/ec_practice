e1c848a822b5a9d855fa487eb94785a2ba1134e3
YUMI <yumip1997@gmail.com>
2022-06-15 10:59:38 +0900

    주문취소, 교환, 반품 객체 삭제

--- backend/src/main/java/com/himart/backend/claim/service/ClaimService.java
+++ backend/src/main/java/com/himart/backend/claim/service/ClaimService.java
@@ -1,9 +1,8 @@
 package com.himart.backend.claim.service;
 
 import com.himart.backend.claim.dto.ClaimDto;
-import com.himart.backend.claim.utils.core.Claim;
-import com.himart.backend.claim.utils.factory.ClaimFactory;
 import com.himart.backend.claim.utils.factory.ClaimType;
+import com.himart.backend.claim.utils.processor.ClaimProcessor;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +11,7 @@ public class ClaimService {
     요청 업무에 따른 클레임 객체를 생성한 후 클레임 로직을 수행한다.
      */
     public void claim(ClaimDto claimDto) throws Exception {
-        Claim claim = ClaimFactory.getClaimByType(claimDto.getClaimType());
-        claim.execute(claimDto);
+        ClaimProcessor claimProcessor = ClaimType.findClaimProcessor(claimDto.getClaimType());
+        claimProcessor.doProcess(claimDto);
     }
 }
--- backend/src/main/java/com/himart/backend/claim/utils/factory/ClaimType.java
+++ backend/src/main/java/com/himart/backend/claim/utils/factory/ClaimType.java
@@ -1,9 +1,5 @@
 package com.himart.backend.claim.utils.factory;
 
-import com.himart.backend.claim.utils.core.Claim;
-import com.himart.backend.claim.utils.core.Exchange;
-import com.himart.backend.claim.utils.core.OrderCancel;
-import com.himart.backend.claim.utils.core.Return;
 import com.himart.backend.claim.utils.processor.AcceptProcessor;
 import com.himart.backend.claim.utils.processor.ClaimProcessor;
 import com.himart.backend.claim.utils.processor.CompleteProcessor;
@@ -19,33 +15,34 @@ import java.util.List;
 public enum ClaimType {
 
     //일반주문취소완료
-    GCC("GCC", OrderCancel.getInstance(CompleteProcessor.getInstance()), Arrays.asList(), Boolean.TRUE),
+    GCC("GCC", CompleteProcessor.getInstance(), Arrays.asList(), Boolean.TRUE),
     //모바일쿠폰주문취소접수
-    MCA("MCA", OrderCancel.getInstance(AcceptProcessor.getInstance()), Arrays.asList(), Boolean.TRUE),
+    MCA("MCA", AcceptProcessor.getInstance(), Arrays.asList(), Boolean.TRUE),
     //모바일쿠폰주문취소완료
-    MCC("MCC", OrderCancel.getInstance(CompleteProcessor.getInstance()), Arrays.asList(), Boolean.FALSE),
+    MCC("MCC", CompleteProcessor.getInstance(), Arrays.asList(), Boolean.FALSE),
 
     //반품접수
-    RA("RA", Return.getInstance(AcceptProcessor.getInstance()), Arrays.asList(), Boolean.TRUE),
+    RA("RA", AcceptProcessor.getInstance(), Arrays.asList(), Boolean.TRUE),
     //반품완료
-    RC("RC", Return.getInstance(CompleteProcessor.getInstance()), Arrays.asList(), Boolean.FALSE),
+    RC("RC", CompleteProcessor.getInstance(), Arrays.asList(), Boolean.FALSE),
     //반품철회
-    RW("RW", Return.getInstance(WithdrawalProcessor.getInstance()), Arrays.asList(), Boolean.TRUE),
+    RW("RW", WithdrawalProcessor.getInstance(), Arrays.asList(), Boolean.TRUE),
 
     //교환접수
-    EA("EA", Exchange.getInstance(AcceptProcessor.getInstance()), Arrays.asList(), Boolean.TRUE),
+    EA("EA", AcceptProcessor.getInstance(), Arrays.asList(), Boolean.TRUE),
     //교환철회
-    EW("EW", Exchange.getInstance(WithdrawalProcessor.getInstance()), Arrays.asList(), Boolean.TRUE);
+    EW("EW", WithdrawalProcessor.getInstance(), Arrays.asList(), Boolean.TRUE);
 
     private final String code;
-    private final Claim claim;
+    private final ClaimProcessor claimProcessor;
     private final List<String> validStatusCodeList;
     private final Boolean claimNoFlag;
 
-    public static ClaimType findClaimType(String code) throws Exception {
+    public static ClaimProcessor findClaimProcessor(String code) throws Exception {
         return Arrays.stream(ClaimType.values())
                 .filter(type -> type.getCode().equals(code))
                 .findFirst()
+                .map(ClaimType::getClaimProcessor)
                 .orElseThrow(() -> new Exception("클레임 타입이 올바르지 않습니다!"));
     };
 }
