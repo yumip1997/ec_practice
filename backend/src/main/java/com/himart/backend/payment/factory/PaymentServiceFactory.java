0a4c7e2c60ed565d6dd9466dab36dd229d92cb4c
YUMI <yumip1997@gmail.com>
2022-06-23 15:35:47 +0900

    팩토리 수정

--- backend/src/main/java/com/himart/backend/payment/factory/PaymentServiceFactory.java
+++ backend/src/main/java/com/himart/backend/payment/factory/PaymentServiceFactory.java
@@ -2,30 +2,26 @@ package com.himart.backend.payment.factory;
 
 import com.himart.backend.payment.code.PaymentType;
 import com.himart.backend.payment.service.PaymentService;
-import com.himart.backend.payment.service.impl.Inicis;
-import com.himart.backend.payment.service.impl.Point;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
-@RequiredArgsConstructor
 public class PaymentServiceFactory {
 
-    private final List<PaymentService> paymentServiceList;
-    private Map<PaymentType, PaymentService> map = new HashMap<>();
+    private final Map<PaymentType, PaymentService> map = new HashMap<>();
 
-    @PostConstruct
-    void init(){
+    public PaymentServiceFactory(List<PaymentService> paymentServiceList){
         paymentServiceList.forEach(paymentService -> map.put(paymentService.getType(), paymentService));
     }
 
-
     public PaymentService getPaymentService(PaymentType paymentType){
-        return map.get(paymentType);
+        try{
+            return map.get(paymentType);
+        }catch (Exception e){
+            throw new IllegalArgumentException("해당하는 결제유형이 없습니다!");
+        }
     }
 }
